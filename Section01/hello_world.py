34db82faff8afdd30838f67e0786c81d1d6c905a
shikha dave <stshikhathakur@gmail.com>
2026-08-05 10:56:52 +0530

    basics

--- Section01/hello_world.py
+++ Section01/hello_world.py
@@ -1,13 +1,32 @@
 print("Hello world")
-print("what is your name?")
+""" print("what is your name?")
 myName = input()
 print("It is good to meet you, " + myName)
 print("The length of your name is: ")
 print(len(myName))
 print("What is your age?")  
 myAge = input()
-print("You will be " + str(int(myAge) + 1) + " in a year.") 
+print("You will be " + str(int(myAge) + 1) + " in a year.")  """
 
-spam = 3
-spam = 'helo'
-spam = spam * 2
+print(2+6)
+print(8/3)
+print(50 % 3)
+a = "Hello world, \n this is a string"
+""" print(a[0])
+print(a[1])
+print(a[2]) 
+print(a[0:5])
+print(a)
+print(a.upper())
+print(len(a))
+print(a[-1])
+print(a[1:3])
+print(a[::5])
+print(a[::-1])
+a = 'Hello World'
+print(a[8:9])
+
+print(a.upper())
+print(a.split()) """
+print(a[2:])
+print('The {1} {0} {2}'.format('brown', 'quick', 'fox'))
